6636b0087d3a15921ecc004578caeaa42585f44b
Laboratorio XXIA - 103 <lab103a@lab.unesc.net>
2024-09-09 20:36:57 -0300

    initial commit

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ScrollView } from "react-native";
+
+export default function TabFourScreen(){
+    return (
+        <ScrollView>
+        </ScrollView>
+    )
+}
