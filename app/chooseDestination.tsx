ef8a1b5d3ea42151a839452c9126f52a37f30b87
guilherme coelho <guilhermeifc7@gmail.com>
2024-10-07 20:43:13 -0300

    feat: Add functionality for displaying a map screen

--- app/chooseDestination.tsx
+++ app/chooseDestination.tsx
@@ -1,31 +1,27 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Alert, StyleSheet, View, Text, TouchableOpacity, TextInput, Image, KeyboardAvoidingView, Platform } from 'react-native';
+import { Alert, StyleSheet, View, Text, TouchableOpacity, SafeAreaView, KeyboardAvoidingView, Platform, TextInput, FlatList, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
+import { useNavigation } from '@react-navigation/native';
 import { UserContext } from '../contexts/UserContext';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { API_KEY } from '../constants/Env';
-import toastHelper from '@/utils/toast';
+import MapViewDirections from 'react-native-maps-directions';
 
-const ChooseDestinationScreen = () => {
+const chooseDestination = () => {
     const { user } = useContext(UserContext);
     const [location, setLocation] = useState<{
         latitude: number;
         longitude: number;
     } | null>(null);
-    const [destination, setDestination] = useState('');
+    const [destination, setDestination] = useState<string>('');
     const [destinationCoordinates, setDestinationCoordinates] = useState<{
         latitude: number;
         longitude: number;
     } | null>(null);
-    const [isTripStarted, setIsTripStarted] = useState(false);
-    const [driver, setDriver] = useState<{
-        name: string;
-        photo: string;
-        coordinates: {
-            latitude: number;
-            longitude: number;
-        }
-    } | null>(null);
+    const [suggestions, setSuggestions] = useState<Array<any>>([]);
+    const [displayCurrentAddress, setDisplayCurrentAddress] = useState('Carregando...');
+    const navigation = useNavigation();
 
     useEffect(() => {
         getCurrentLocation();
@@ -34,122 +30,199 @@ const ChooseDestinationScreen = () => {
     const getCurrentLocation = async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
-            Alert.alert('Permission denied', 'Allow the app to use the location services');
+            Alert.alert('Permission denied', 'Allow the app to use the location services', [
+                { text: 'Cancel', style: 'cancel' },
+                { text: 'OK' },
+            ]);
             return;
         }
 
         const { coords } = await Location.getCurrentPositionAsync();
         if (coords) {
-            setLocation({ latitude: coords.latitude, longitude: coords.longitude });
+            const { latitude, longitude } = coords;
+            setLocation({ latitude, longitude });
+
+            let response = await Location.reverseGeocodeAsync({ latitude, longitude });
+            for (let item of response) {
+                let address = `${item.street}, ${item.city}`;
+                setDisplayCurrentAddress(address);
+            }
         }
     };
 
-    const handleSearch = async () => {
+    // Função para integrar com a API do Google e buscar sugestões
+    const fetchSuggestions = async (input: string) => {
+        if (!input) return;
+        const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${API_KEY}&location=${location?.latitude},${location?.longitude}&radius=2000`;
         try {
-            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(destination)}&key=${API_KEY}`);
-            console.log(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(destination)}&key=${API_KEY}`)
+            const response = await fetch(url);
             const data = await response.json();
-
-            if (data.results.length > 0) {
-                const { geometry: { location } } = data.results[0];
-                setDestinationCoordinates(location);
-                Alert.alert("teste",JSON.stringify(location))
-            } else {
-                Alert.alert('Endereço não encontrado', 'Tente um endereço diferente.');
+            if (data.status === 'OK') {
+                setSuggestions(data.predictions);
             }
         } catch (error) {
-            console.error(error);
-            Alert.alert('Erro', 'Não foi possível buscar o endereço.');
+            console.error('Erro ao buscar sugestões:', error);
         }
     };
 
-    const confirmTrip = () => {
-        setIsTripStarted(true);
-        // Mock do motorista aceitando a viagem
-        setTimeout(() => {
-            if (location) {
-                setDriver({
-                    name: 'Motorista',
-                    photo: 'https://randomuser.me/api/portraits/men/1.jpg',
-                    coordinates: {
-                        latitude: location.latitude + 0.001,
-                        longitude: location.longitude + 0.001
-                    }
-                });
-            } else {
-                toastHelper.error("Não há localização", "Selecione um local de partida")
-            }
-        }, 5000);
+    // Função ao selecionar um destino da lista
+    const handleSelectSuggestion = async (placeId: string) => {
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${API_KEY}`;
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            const { lat, lng } = data.result.geometry.location;
+            setDestinationCoordinates({ latitude: lat, longitude: lng });
+            setSuggestions([]); // Limpar as sugestões após a escolha
+        } catch (error) {
+            console.error('Erro ao buscar detalhes do lugar:', error);
+        }
+    };
+
+    // Função para calcular a região do mapa com origem e destino
+    const calculateRegion = () => {
+        if (!location || !destinationCoordinates) return null;
+
+        const latitudes = [location.latitude, destinationCoordinates.latitude];
+        const longitudes = [location.longitude, destinationCoordinates.longitude];
+
+        const minLatitude = Math.min(...latitudes);
+        const maxLatitude = Math.max(...latitudes);
+        const minLongitude = Math.min(...longitudes);
+        const maxLongitude = Math.max(...longitudes);
+
+        const latitudeDelta = (maxLatitude - minLatitude) + 0.1; // Delta com margem extra
+        const longitudeDelta = (maxLongitude - minLongitude) + 0.1;
+
+        return {
+            latitude: (minLatitude + maxLatitude) / 2,
+            longitude: (minLongitude + maxLongitude) / 2,
+            latitudeDelta,
+            longitudeDelta
+        };
     };
 
     return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-            keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}
-        >
-
-            {location && (
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: location.latitude,
-                        longitude: location.longitude,
-                        latitudeDelta: 0.01,
-                        longitudeDelta: 0.01,
-                    }}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={styles.container}>
+                <KeyboardAvoidingView
+                    style={styles.container}
+                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                    keyboardVerticalOffset={Platform.OS === 'ios' ? 10 : 0}
                 >
-                    <Marker coordinate={location} title="Sua localização" />
-                    {destinationCoordinates && (
-                        <Marker coordinate={destinationCoordinates} title="Destino" pinColor="blue" />
+                    {/* Campo de destino no topo */}
+                    <View style={styles.topInputContainer}>
+                        <TouchableOpacity onPress={() => navigation.goBack()}>
+                            <Text>
+                                <Icon name="arrow-left" size={24} color="#fff" />  {/* Botão de Voltar */}
+                            </Text>
+                        </TouchableOpacity>
+                        <TextInput
+                            value={destination}
+                            onChangeText={(text) => {
+                                setDestination(text);
+                                fetchSuggestions(text); // Buscar sugestões à medida que o texto muda
+                            }}
+                            style={styles.input}
+                            placeholder="Digite seu destino"
+                            placeholderTextColor="#ccc"
+                        />
+                    </View>
+
+                    {/* Lista de sugestões */}
+                    {suggestions.length > 0 && (
+                        <FlatList
+                            data={suggestions}
+                            keyExtractor={(item) => item.place_id}
+                            renderItem={({ item }) => (
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        handleSelectSuggestion(item.place_id)
+                                        setDestination(item.description)
+                                    }}
+                                    style={styles.suggestionItem}
+                                >
+                                    <Text style={styles.suggestionText}>{item.description}</Text>
+                                </TouchableOpacity>
+                            )}
+                            style={styles.suggestionsList}
+                        />
                     )}
-                    {isTripStarted && driver && (
-                        <Marker coordinate={driver.coordinates} title={driver.name} image={{ uri: driver.photo }} />
+
+                    {/* Mapa */}
+                    {location && (
+                        <MapView
+                            style={styles.map}
+                            region={calculateRegion() || {
+                                latitude: location.latitude,
+                                longitude: location.longitude,
+                                latitudeDelta: 0.05,
+                                longitudeDelta: 0.05,
+                            }}
+                            loadingEnabled={true}
+                        >
+                            {/* Marcador da localização atual */}
+                            <Marker coordinate={location} title="Sua localização" description={displayCurrentAddress} />
+
+                            {/* Marcador do destino escolhido */}
+                            {destinationCoordinates && (
+                                <Marker coordinate={destinationCoordinates}
+                                    title="Destino"
+                                    description={destination} />
+                            )}
+                            <MapViewDirections
+                                origin={location}
+                                destination={destinationCoordinates || location}
+                                strokeWidth={4}
+                                strokeColor='#111111'
+                                apikey={API_KEY}
+                            />
+                        </MapView>
                     )}
-                </MapView>
-            )}
-
-            <View style={styles.inputContainer}>
-                <TextInput
-                    value={destination}
-                    onChangeText={setDestination}
-                    style={styles.input}
-                    placeholder="Digite sua mensagem"
-                    placeholderTextColor="#ccc"
-                    multiline // Permitir quebra de linha // Limitar o crescimento do input
-                />
-                <TouchableOpacity onPress={handleSearch} style={styles.button}>
-                    <Text style={styles.buttonText}>Enviar</Text>
-                </TouchableOpacity>
-            </View>
-
-            {destinationCoordinates && !isTripStarted && (
-                <TouchableOpacity style={styles.confirmButton} onPress={confirmTrip}>
-                    <Text style={styles.buttonText}>Confirmar Viagem</Text>
-                </TouchableOpacity>
-            )}
-
-            <View style={styles.userInfo}>
-                <Image source={{ uri: user?.photo }} style={styles.userPhoto} />
-                <Text style={styles.userName}>{user?.username}</Text>
-            </View>
-        </KeyboardAvoidingView>
-
+                    <View style={styles.buttonsContainer}>
+                        <TouchableOpacity
+                            style={styles.startTripButton}
+                            onPress={() => { }}>
+                            <Text style={styles.buttonText}>
+                                {'Solicitar Viagem'}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
+        </TouchableWithoutFeedback>
     );
 };
 
 const styles = StyleSheet.create({
+
+    startTripButton: {
+        backgroundColor: '#4CAF50',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonsContainer: {
+        position: 'absolute',
+        bottom: 40,
+        left: 20,
+        right: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        gap: 10,
+    },
     container: {
         flex: 1,
         backgroundColor: '#000',
     },
-    map: {
-        flex: 1,
-        marginBottom: 100,
-    },
-    inputContainer: {
+    topInputContainer: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
         flexDirection: 'row',
         alignItems: 'center',
+        zIndex: 10,
     },
     input: {
         flex: 1,
@@ -158,45 +231,37 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 10,
         marginRight: 10,
-        maxHeight: 100,
     },
     button: {
-        backgroundColor: '#4CAF50',
+        backgroundColor: '#44EAC3',
         padding: 10,
-        borderRadius: 5,
-    },
-    confirmButton: {
-        position: 'absolute',
-        bottom: 40,
-        left: 20,
-        right: 20,
-        backgroundColor: '#FF9800',
-        padding: 15,
-        borderRadius: 5,
-        alignItems: 'center',
+        borderRadius: 10,
     },
     buttonText: {
-        color: '#fff',
+        color: '#000',
         fontSize: 16,
     },
-    userInfo: {
+    suggestionsList: {
         position: 'absolute',
-        top: 40,
+        top: 80,
         left: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
+        right: 20,
+        zIndex: 11,
+        backgroundColor: '#333',
+        borderRadius: 10,
     },
-    userPhoto: {
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        marginRight: 10,
+    suggestionItem: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#444',
     },
-    userName: {
+    suggestionText: {
         color: '#fff',
-        fontSize: 18,
-        fontWeight: 'bold',
+    },
+    map: {
+        flex: 1,
+        marginTop: 80, // Ajustar o mapa para não sobrepor os campos
     },
 });
 
-export default ChooseDestinationScreen;
+export default chooseDestination;
